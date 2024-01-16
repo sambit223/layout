70b589b0b39d4a83fdec37f3ce515ba0e34bb234
unknown <sambittripathy200@gmail.com>
2024-01-16 23:34:49 +0530

    simple layout commit

---
+++ src/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Homepage.css'
+
+const Homepage = () => {
+    return (
+        <body className='container'>
+            <header id='pageHeader'>Page Header</header>
+            <article id='mainArticle'>Main Article</article>
+            <nav id='mainNav'>NavBar</nav>
+            <aside id='sideads'>Ads</aside>
+            <footer id='pageFooter'>footer</footer>
+        </body>
+    )
+}
+
+export default Homepage
